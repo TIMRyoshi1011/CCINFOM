c158983245fbcf0813b9fdf4e3f9dc114052bb71
JustineValdes <164148945+Spinutch@users.noreply.github.com>
2025-11-17 20:14:37 +0800

    added a run file for macOs

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Compile the Java file
+javac -cp lib/mysql-connector-j-9.5.0.jar src/TheaterShows.java -d out
+
+# Run the Java program
+java -cp "lib/mysql-connector-j-9.5.0.jar:out" TheaterShows
