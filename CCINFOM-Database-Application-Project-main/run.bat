3941f844099d8df3b4d4d8892fe01a978040211a
JustineValdes <164148945+Spinutch@users.noreply.github.com>
2025-11-18 19:03:49 +0800

    Deleted the duplicate files

--- CCINFOM-Database-Application-Project-main/run.bat
+++
@@ -1,3 +0,0 @@
-javac -cp lib/mysql-connector-j-9.5.0.jar src/*.java -d out
-
-java -cp "lib/mysql-connector-j-9.5.0.jar;out" Main
--- CCINFOM-Database-Application-Project-main/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compile the Java file
-javac -cp lib/mysql-connector-j-9.5.0.jar src/*.java -d out
-
-# Run the Java program
-java -cp "lib/mysql-connector-j-9.5.0.jar;out" Main
