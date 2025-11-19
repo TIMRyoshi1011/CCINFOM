072a6a469845238e384ee48300ea22a35af00f84
coder21-art <santinosuarez822@gmail.com>
2025-11-19 11:15:26 +0800

    Updated DB Creation

---
+++ GeneralSQLStatements.sql
@@ -0,0 +1,26 @@
+select *
+from booking;
+
+select *
+from customers;
+
+select *
+from seat;
+
+select *
+from seat_booking;
+
+select *
+from shows;
+
+select *
+from staff;
+
+select *
+from staff_assignment;
+
+select *
+from theaters;
+
+select *
+from theater_shows;
